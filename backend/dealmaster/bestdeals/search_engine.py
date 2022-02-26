417ac24fd98a95118b85c6acc3a1b5ecf61b75c5
Anto Ark <tuyenngo7bh@gmail.com>
2022-02-26 02:48:47 -0800

    upload search_engine.py, files in bestdeals

--- backend/dealmaster/bestdeals/search_engine.py
+++ backend/dealmaster/bestdeals/search_engine.py
@@ -4,3 +4,11 @@ query = 'hp laptop'
 
 for j in search(query, tld="co.in", num=10, stop=10, pause=2):
     print(j)
+
+
+def search_request(request):
+    url_list = []
+    for j in search(request, tld="co.in", num=10, stop=10, pause=2):
+        url_list.append(j)
+
+    return url_list
--- backend/dealmaster/bestdeals/views.py
+++ backend/dealmaster/bestdeals/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import search_engine
+
 
 # Create your views here.
+def home_view(request):
+
+    return render(request, 'main.html')
+
