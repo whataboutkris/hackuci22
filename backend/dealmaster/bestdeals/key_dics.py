a80bd150441c63acc32c601fe54896f61fdd4f56
Anto Ark <tuyenngo7bh@gmail.com>
2022-02-26 15:09:10 -0800

    update search engine

---
+++ backend/dealmaster/bestdeals/key_dics.py
@@ -0,0 +1,33 @@
+
+car = {'Toyota': ['corolla', 'camry', 'mirai', 'prius', 'avalon', 'rav4', 'tundra', 'tacoma', 'sienna'],
+       'Nissan': ['versa', 'sentra', 'altima', 'maxima', 'leaf', 'kicks', 'rogue', 'murano'],
+       'Honda': ['civic', 'accord', 'odyssey'],
+       'Chevrolet': ['malibu', 'sparks', 'colorado', 'silverado', 'trax', 'trailblazer', 'equinox'],
+       'Ford': ['focus', 'escor', 'fiesta', 'modeo'],
+       # 'Mercedes-Benz': [],
+       # 'Jeep': [],
+       # 'Subaru': [],
+       # 'Cadillac': [],
+       # 'Volkswagen': [],
+       # 'Lexus': []
+       }
+laptop = ['Samsung',
+          'Apple',
+          'MSI',
+          'Asus',
+          'Acer',
+          'Hp',
+          'Dell',
+          'Microsoft',
+          'Chrome Book']
+
+shoes = ['Nike', 'Adidas', 'Vans', 'Converse', 'Sketcher']
+
+book_genres = ['novel', 'sci-fi', 'horror', 'mystery',
+               'comic', 'humour', 'religion', 'self-help'
+               'crime', 'philosophy', 'science', 'encyclopedia',
+               'garden', 'dictionary', 'cookbook', 'craft',
+               'art']
+
+
+
